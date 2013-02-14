c374ca05cd3a9169ec2c5e6c5ec1a5f5e7b69164
gustavo <gustavo@CViper.(none)>
2013-02-13 22:35:30 -0200

    removendo projeto unico

--- rsa_test.py
+++
@@ -1,7 +0,0 @@
-from Crypto import Random
-from Crypto.PublicKey import RSA
-
-private = RSA.generate(1024, Random.new().read)
-public = private.publickey()
-print private.exportKey().replace('\n','\\n')
-print public.exportKey().replace('\n','\\n')
